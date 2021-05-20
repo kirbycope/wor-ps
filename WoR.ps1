8f6dbab395e4dfb773ec4449ff0bb0384fc4d024
Tim Cope <kirbycope@gmail.com>
2021-05-20 15:21:24 -0700

    wip

--- WoR.ps1
+++ WoR.ps1
@@ -1,4 +1,4 @@
-﻿# Note: Editing the WIM file requires that this script is ran with elevated priviledges (as an Administrator)
+﻿#Requires -RunAsAdministrator
 
 $startTime = Get-Date
 
@@ -128,17 +128,17 @@ Write-Host "Complete!" -ForegroundColor Green
 
 Write-Host "Removing the bloatware..." -ForegroundColor Yellow
 #takeown /a /r /d Y /f "$downloadsFolder\wim" > null
-Remove-Item "$downloadsFolder\wim\Windows\System32\Recovery" -Recurse > null
-Remove-Item "$downloadsFolder\wim\Windows\System32\BingMaps.dll" > null
-Remove-Item "$downloadsFolder\wim\Windows\SysWoW64\OneDriveSetup.exe" > null
-Remove-Item "$downloadsFolder\wim\Windows\SysWoW64\BingMaps.dll" > null
-Remove-Item "$downloadsFolder\wim\Windows\SysArm32\BingMaps.dll" > null
+Remove-Item "$downloadsFolder\wim\Windows\System32\Recovery" -Recurse
+Remove-Item "$downloadsFolder\wim\Windows\System32\BingMaps.dll"
+Remove-Item "$downloadsFolder\wim\Windows\SysWoW64\OneDriveSetup.exe"
+Remove-Item "$downloadsFolder\wim\Windows\SysWoW64\BingMaps.dll"
+Remove-Item "$downloadsFolder\wim\Windows\SysArm32\BingMaps.dll"
 Remove-Item "$downloadsFolder\wim\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" > null
-Remove-Item "$downloadsFolder\wim\Program Files (x86)\Internet Explorer" -Recurse > null
-Remove-Item "$downloadsFolder\wim\Program Files (Arm)\Internet Explorer" -Recurse > null
-Remove-Item "$downloadsFolder\wim\Program Files (Arm)\Windows Defender" -Recurse > null
-Remove-Item "$downloadsFolder\wim\Program Files (Arm)\Windows Mail" -Recurse > null
-Remove-Item "$downloadsFolder\wim\Program Files\Internet Explorer" -Recurse > null
+Remove-Item "$downloadsFolder\wim\Program Files (x86)\Internet Explorer" -Recurse
+Remove-Item "$downloadsFolder\wim\Program Files (Arm)\Internet Explorer" -Recurse
+Remove-Item "$downloadsFolder\wim\Program Files (Arm)\Windows Defender" -Recurse
+Remove-Item "$downloadsFolder\wim\Program Files (Arm)\Windows Mail" -Recurse
+Remove-Item "$downloadsFolder\wim\Program Files\Internet Explorer" -Recurse
 Write-Host "Complete!" -ForegroundColor Green
 
 Write-Host "Saving WIM file..." -ForegroundColor Yellow
